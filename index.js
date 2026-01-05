221d7d9ae3d0fc9d0f5acde42c2f3d638686aeb7
greengrassgood <108985841+greengrassgood@users.noreply.github.com>
2026-01-05 16:13:29 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("OK");
+});
+
+server.listen(process.env.PORT || 3000);
